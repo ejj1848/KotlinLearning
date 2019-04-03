02fbc64b522f241fa0cc41399474b97a9ab1b550
Eric Johnson <eric.johnson@Astontech.com>
2019-04-03 12:31:42 -0500

    Loop lesson

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,68 @@
 import domain.Person
 import domain.Question
-import java.lang.NumberFormatException
+import java.util.*
 
 fun main() {
-    println("Hello World")
 
+
+
+}
+
+fun loops() {
+    //for loops
+    for (i in 1..10) {
+        println(i)
+    }
+    //non inclusive
+    for (i in 1 until 10) {
+        println(i)
+    }
+
+    //stepping
+    for (i in 1..10 step 2) {
+        println(i)
+    }
+    // descending
+    for (i in 10 downTo 1 step 2) {
+        println(i)
+    }
+
+    // List iteration
+    var numbers = listOf(1, 2, 2, 3, 4, 56, 7)
+
+    for (i in numbers) {
+        println(i)
+    }
+    for((index, element) in numbers.withIndex()){
+        println("$element at $index")
+    }
+
+    //Map iteration
+    var ages = TreeMap<String, Int>()
+    ages["Kevin"] = 55
+    ages["Eric"] = 30
+    ages["Bipin"] =40
+
+    for ((name, age) in ages){
+        println("$name is $age")
+    }
+
+
+
+}
+
+fun printName(name: String) {
+    println(name)
+}
+
+fun printPerson() {
     var eric = Person("Eric")
     eric.name = "Eric"
-    println(eric.name)
     eric.display()
     eric.displayWithLambda(::printName)
+}
+
+fun questionLesson() {
 
     var q = Question()
     q.printResult()
@@ -17,27 +70,9 @@ fun main() {
     // Try Catch
     val number: Int? = try {
         Integer.parseInt(q.answer)
-    }catch (e:NumberFormatException){
+    } catch (e: NumberFormatException) {
         null
     }
 
     println(number)
-
-    //for loops
-    for (i in 1..10){
-        println(i)
-    }
-    //stepping
-    for (i in 1..10 step 2){
-        println(i)
-    }
-    // descending
-    for (i in 10 downTo 1 step 2){
-        println(i)
-    }
-
-}
-
-fun printName(name: String) {
-    println(name)
 }
